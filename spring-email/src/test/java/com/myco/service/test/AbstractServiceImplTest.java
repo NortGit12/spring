c371b7a0b7cba8b6da9f12ebea45987ae391d424
Jeff Norton <jnorton@ancestry.com>
2013-07-12 16:51:16 -0600

    Some tidy up work

--- spring-email/src/test/java/com/myco/service/test/AbstractServiceImplTest.java
+++ spring-email/src/test/java/com/myco/service/test/AbstractServiceImplTest.java
@@ -6,7 +6,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {ServiceTestConfig.class})
-//@ActiveProfiles("dev")
 public abstract class AbstractServiceImplTest {
 
     // Use this when testing database access
--- spring-email/src/test/java/com/myco/service/test/ServiceTestConfig.java
+++ spring-email/src/test/java/com/myco/service/test/ServiceTestConfig.java
@@ -3,15 +3,10 @@ package com.myco.service.test;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
-//import org.springframework.context.annotation.Import;
-//import org.springframework.context.annotation.Profile;
-//import com.ancestry.dna.fulfillment.service.config.ServiceConfiguration;
 
 @Configuration
 @ComponentScan(basePackages={"com.myco"})
-//@Import(ServiceConfiguration.class)
 @ImportResource("classpath:/spring/context.xml")
-//@Profile("dev")
 public class ServiceTestConfig {
 
 }
